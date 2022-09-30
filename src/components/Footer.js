b2ba745a6a570bc9b15e770b1064887b52d7a263
NerVod <benjamin.jeannerot@gmail.com>
2022-09-30 18:24:51 +0200

    input email avec contrôle bonne saisie utilisateur

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,35 @@ import '../styles/Footer.css'
 function Footer() {
 	const [inputValue, setInputValue] = useState('')
 
+	function checkValidEmail (value) {
+		if (!value.includes('@')) {
+			alert('L\'email ne contient pas "@" ! ')
+		} else {
+			setInputValue(value)
+		}
+	}
+	function validateEmail (value) {
+		if (!value.includes('@')) {
+			alert('L\'email ne contient pas "@" et n\est pas pris en compte! ')
+		} else {
+			alert(`${value} enregistré comme votre email !`)
+		}
+	}
+
 	return (
 		<footer className='lmj-footer'>
 			<div className='lmj-footer-elem'>
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
 			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+			<input 
+				type="email" 
+				placeholder="votre email" 
+				value={inputValue}
+				onChange={(e) => setInputValue(e.target.value)}
+				onBlur={() => checkValidEmail(inputValue)}
+				></input>
+			<button onClick={() => validateEmail(inputValue)}>Valider l'Email</button>
 		</footer>
 	)
 }
