a80eeb7ce0538542a2f1c047b806eb38d1f153b1
NerVod <benjamin.jeannerot@gmail.com>
2022-09-30 15:24:49 +0200

    function event handler on click plante

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,9 +1,17 @@
 import '../styles/PlantItem.css'
 import CareScale from './CareScale'
 
+function handleClick(e) {
+    // console.group("👉 Détection d'événement")
+    // console.log("🌟 Reaction au click 🌟 ")
+    console.log("🌟 pour l'évent e :", e)
+    // console.log(`🌟 Vous avez cliqué sur ${name}   🌟`)
+}
+
+
 function PlantItem({ id, cover, name, water, light }) {
 	return (
-		<li key={id} className='lmj-plant-item'>
+		<li key={`${id}`} className='lmj-plant-item' onClick={(e) => handleClick(e)} >
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
